3a498953b37fdd59e909e7ec465b2b3fe6377e70
Khaoula <kwlsf04@gmail.com>
2026-04-10 11:58:44 +0100

    Updated Job Service API

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    job_name = models.CharField(max_length=255)  # بدل title ✔
+    job_name = models.CharField(max_length=255) 
     description = models.TextField()
     company = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
